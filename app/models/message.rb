b63aafc93a2ba39d5bc60e0707bd0dd9b86da28c
sutatin <sutatinsutatin+cloud9@gmail.com>
2016-01-05 08:13:24 +0000

    add age to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20}, presence: true
     #内容は必須入力かつ２０文字以内
     validates :body , length: { minimum: 2, maximum: 20},presence: true
+    #内容は0以上の数値のみ,とりあえず必須制御は指定なしなのでfalseで
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0}
 end
